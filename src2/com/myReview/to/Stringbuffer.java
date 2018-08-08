fd35942c1d31e4292301e9ccd89def1b10e3c318
Mr_Ason <895886594@qq.com>
2018-08-08 11:29:05 +0800

    StringBuffer

---
+++ src2/com/myReview/to/Stringbuffer.java
@@ -0,0 +1,28 @@
+/**   
+ * projectName: MyJava   
+ * fileName: Stringbuffer.java   
+ * packageName: com.myReview.to   
+ * date: 2018年8月8日上午10:25:49   
+ * copyright(c) 2017-2020 xxx公司  
+ */ 
+package com.myReview.to;
+
+/**     
+ * @title: Stringbuffer.java   
+ * @package com.myReview.to   
+ * @description: TODO  
+ * @author: LiuJiDong  
+ * @date: 2018年8月8日 上午10:25:49   
+ * @version: V1.0     
+*/
+public class Stringbuffer {
+	public static void main(String[] args){
+		StringBuffer  buf = new StringBuffer();
+		buf.append("hello ").append("your");
+		buf.append("world!!");
+		System.out.println(buf);
+		String str = buf.delete(6, 10).toString();
+		System.out.println(str);
+	}
+
+}
